91aa496e6b940314c8d6a8ac7324fb39cb2d7b12
mmingoia <michaelmingoia77@gmail.com>
2020-07-13 14:06:50 -0500

    Updated SQL Tables and Imported

--- schema.sql
+++ schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE dept_emp (
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE titles (
   emp_no INT NOT NULL,
@@ -46,6 +46,5 @@ CREATE TABLE titles (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
-SELECT * FROM departments;
