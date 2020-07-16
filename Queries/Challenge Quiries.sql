683218f21431f9df85b66a1e81930a7b919def5c
mmingoia <michaelmingoia77@gmail.com>
2020-07-16 12:32:15 -0500

    Challenge Deliverables

---
+++ Queries/Challenge Quiries.sql
@@ -0,0 +1,87 @@
+-- Create Table of Number of Retiring Employees by Title - Deliverable 1
+SELECT e.emp_no,
+	e.first_name,
+    e.last_name,
+	titles.title,
+	titles.to_date,
+	de.from_date,
+	s.salary
+INTO RetiringByTitle
+FROM employees as e
+INNER JOIN titles
+ON (e.emp_no = titles.emp_no)
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	 AND (de.to_date = '9999-01-01');
+
+SELECT * FROM retiringbytitle;
+
+-- Partition the data to show only most recent title per employee  - Deliverable 1
+SELECT emp_no,
+	first_name,
+    last_name,
+	title,
+	from_date,
+	salary
+INTO retiringByTitleNoDups
+FROM 
+ (SELECT rbt.emp_no,
+	rbt.first_name,
+    rbt.last_name,
+	rbt.title,
+	rbt.from_date,
+    rbt.salary, ROW_NUMBER() OVER
+ (PARTITION BY (emp_no)
+ ORDER BY rbt.to_date DESC) rn
+ FROM retiringbytitle as rbt
+ ) tmp WHERE rn = 1
+ORDER BY emp_no;
+
+SELECT * FROM retiringbytitlenodups;
+
+-- Create Table of potential Mentors - Deliverable 2
+SELECT e.emp_no,
+	e.first_name,
+    e.last_name,
+	titles.title,
+	de.from_date,
+	de.to_date
+INTO mentorList
+FROM employees as e
+INNER JOIN titles
+ON (e.emp_no = titles.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+     AND (titles.to_date = '9999-01-01'); 
+-- Above code results in duplicates need to remove duplicates
+
+SELECT * FROM mentorlist;
+
+-- Partition the data to show remove duplicates - Deliverable 2
+SELECT emp_no,
+	first_name,
+    last_name,
+	title,
+	from_date,
+	to_date
+INTO MentorListNoDups
+FROM 
+ (SELECT ml.emp_no,
+	ml.first_name,
+    ml.last_name,
+	ml.title,
+	ml.from_date,
+    ml.to_date, ROW_NUMBER() OVER
+ (PARTITION BY (emp_no)
+ ORDER BY ml.to_date DESC) rn
+ FROM mentorlist as ml
+ ) tmp WHERE rn = 1
+ORDER BY emp_no;
+
+SELECT * FROM mentorlistnodups;
+
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # Pewlett-Hackard-Analysis
+
 UTDA Module 07
+
+## Challenge Work
+
+Delivering Results
+
+1. A README.md in the form of a technical report that details your analysis and findings.
+2. Technical Analysis Deliverable 1: Number of Retiring Employees by Title. You will create three new tables, one showing number of "titles" retiring, one showing number of employees with each title, and one showing a list of current employees born between Jan. 1, 1952 and Dec. 31, 1955. New tables are exported as CSVs. ----RetiringbyTitle.csv and retiringByTitleNoDups.csv
+3. Technical Analysis Deliverable 2: Mentorship Eligibility. A table containing employees who are eligible for the mentorship program You will submit your table and the CSV containing the data (and the CSV containing the data)
+
+## Technical Report
+
+In your first paragraph, introduce the problem that you were using data to solve.
+
+In your second paragraph, summarize the steps that you took to solve the problem, as well as the challenges that you encountered along the way. This is an excellent spot to provide examples and descriptions of the code that you used.
+
+for deliverable 1 used partition, created 1 table with all the data then partitioned it.  Added the "to_date" from titles in order to partition down to the most recent title. (33,118 near retirement)
+
+![ERD](EmployeeDB.png)
+
+In your final paragraph, share the results of your analysis and discuss the data that you’ve generated. Have you identified any limitations to the analysis? What next steps would you recommend?
